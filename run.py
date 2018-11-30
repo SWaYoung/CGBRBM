00796a972fbb49275f7b2808eb78d8cf1f35534b
Sean Young <ws704603301@gmail.com>
2018-11-30 20:24:44 +0800

    加入DBSCAN，加入spy用以判别整体正确率

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 import numpy as np
 import tensorflow as tf
 from sklearn import metrics
+from sklearn.cluster import DBSCAN
 import data_util
 from tfrbm.cgbrbm import CGBRBM
 import matplotlib.pyplot as plt
@@ -16,10 +17,7 @@ import matplotlib.pyplot as plt
 from MulticoreTSNE import MulticoreTSNE as TSNE
 import pickle
 
-# define
-base_data_dir = '../Data/Image_Classification'
-
-
+# define functions
 def get_time_dif(start_time):
     """获取已使用时间"""
     end_time = time.time()
@@ -33,22 +31,60 @@ def print_tsne(data, label, color, axis):
         m = "^"
         axis.scatter(_[0], _[1], c=c, marker=m, label=label)
     print(label, 'finished')
+    
+#%% define variables
+base_data_dir = '../Data/Image_Classification'
 
+chanel_increase=16
+filter_height=5  # 卷积核尺寸
+filter_width=5  # 卷积核尺寸
+offset_height=80
+offset_width=270
+target_height=700
+target_width=700
+resize_height=64
+resize_width=64
+resize_method=0
+num_each_cat=1000
+total_cat = None
+total_spy = 6
+total_input = None
 
-print("loading training and validation data...")
+spy_batch_size = 6
+batch_size = 30
+n_visible=(resize_height / 4 * resize_width / 4) * (chanel_increase * 2)
+n_hidden=1000
+
+#%% loading data & initilize cgbrbm...
+print("loading data & initilize cgbrbm...")
 start_time = time.time()
-cgbrbm = CGBRBM(n_visible=8192, n_hidden=800, chanel_increase=16)
-x_train, y_train = data_util.create_raw_data(base_data_dir)
+cgbrbm = CGBRBM(int(n_visible), 
+                n_hidden, 
+                chanel_increase,
+                filter_height,  # 卷积核尺寸
+                filter_width,  # 卷积核尺寸
+                offset_height,
+                offset_width,
+                target_height,
+                target_width,
+                resize_height,
+                resize_width,
+                resize_method)
+
+x_train, y_train, total_cat = data_util.create_raw_data(base_data_dir, num_each_cat)
+x_spy, y_spy = data_util.create_spy_data(base_data_dir, total_spy)
+
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
-#
+#%% Training and evaluating...
 print('Training and evaluating...')
 start_time = time.time()
-errs = cgbrbm.cfit(x_train, y_train, n_epoches=20, batch_size=30)
+errs = cgbrbm.cfit(x_train, y_train, n_epoches=10, batch_size=batch_size)
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
+#%% Print error plot
 print('Print error plot')
 start_time = time.time()
 plt.plot(errs)
@@ -56,30 +92,44 @@ plt.show()
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
-print('Getting hidden layers...')
+#%% Getting hidden layers & labels...
+print('Getting hidden layers & labels...')
 start_time = time.time()
-hidden_layers = np.zeros((3000, 800))
+total_input = num_each_cat * total_cat + total_spy
+hidden_layers = np.zeros((total_input, n_hidden))
 hidden_layer_counter = 0
 for file in os.listdir('./pickle')[0:100]:
     with open('./pickle/' + file, 'rb') as p:
         temp = pickle.load(p)
-        hidden_layers_batch = cgbrbm.ctransform(temp, batch_size=30)  # 得到hidden layer
+        hidden_layers_batch = cgbrbm.ctransform(temp, batch_size=batch_size)  # 得到hidden layer
         for i in range(len(hidden_layers_batch)):
             hidden_layers[hidden_layer_counter] = hidden_layers_batch[i]
             hidden_layer_counter += 1
-time_dif = get_time_dif(start_time)
-print("Time usage:", time_dif)
-
-print('Getting labels...')
-start_time = time.time()
+            
+#%%
 label_list = []
 for file in os.listdir('./pickle')[100:200]:
     with open('./pickle/' + file, 'rb') as p:
         temp = pickle.load(p)
-        label_list.extend([temp[i].decode("utf-8") for i in range(30)])
+        label_list.extend([temp[i].decode("utf-8") for i in range(batch_size)])
+        
+#%%
+spy_hidden_layers = cgbrbm.ctransform_raw(x_spy, y_spy, spy_batch_size)
+
+for i in range(len(spy_hidden_layers)):
+    hidden_layers[hidden_layer_counter] = spy_hidden_layers[i]
+    hidden_layer_counter += 1
+
+
+#%%
+for file in os.listdir('./pickle')[200 + int(total_spy / spy_batch_size):]:
+    with open('./pickle/' + file, 'rb') as p:
+        temp = pickle.load(p)
+        label_list.extend([temp[i].decode("utf-8") for i in range(spy_batch_size)])
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
+#%% TSNE
 print('TSNE...')
 start_time = time.time()
 embedded_dim = 2
@@ -89,32 +139,114 @@ new_dim_points = tsne.fit_transform(hidden_layers)
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
-print('get points seperated...')
+#%%
+print('get points seperated by label...')
 start_time = time.time()
-point_list_16 = []
+label_16 = []
 for i in range(len(label_list)):
     if label_list[i][29] == '1':
-        point_list_16.append(new_dim_points[i])
-point_list_64 = []
+        label_16.append(np.append(np.copy(new_dim_points[i]), [i]))
+label_16.append(np.append(np.copy(new_dim_points[-6]), [total_input - 6]))
+label_16.append(np.append(np.copy(new_dim_points[-5]), [total_input - 5]))
+
+label_64 = []
 for i in range(len(label_list)):
     if label_list[i][29] == '6':
-        point_list_64.append(new_dim_points[i])
-point_list_Q = []
+        label_64.append(np.append(np.copy(new_dim_points[i]), [i]))
+label_64.append(np.append(np.copy(new_dim_points[-4]), [total_input - 4]))
+label_64.append(np.append(np.copy(new_dim_points[-3]), [total_input - 3]))
+    
+label_Q = []
 for i in range(len(label_list)):
     if label_list[i][29] == 'Q':
-        point_list_Q.append(new_dim_points[i])
+        label_Q.append(np.append(np.copy(new_dim_points[i]), [i]))
+label_Q.append(np.append(np.copy(new_dim_points[-2]), [total_input - 2]))
+label_Q.append(np.append(np.copy(new_dim_points[-1]), [total_input - 1]))
+        
+label_16 = np.array(label_16)
+label_64 = np.array(label_64)
+label_Q = np.array(label_Q)
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
 
-print('Plot TSNE...')
+#%%
+print('DBSCAN...')
 start_time = time.time()
-fig, ax = plt.subplots()
-print_tsne(data=point_list_16, label='16', color='dodgerblue', axis=ax)
-print_tsne(data=point_list_64, label='64', color='red', axis=ax)
-print_tsne(data=point_list_Q, label='Q', color='gold', axis=ax)
+dbscan_label = DBSCAN(eps=2, min_samples=4).fit_predict(new_dim_points)
+plt.scatter(new_dim_points[:, 0], new_dim_points[:, 1], c=dbscan_label)
 plt.show()
 time_dif = get_time_dif(start_time)
 print("Time usage:", time_dif)
+
+#%%
+print('get points seperated by dbscan...')
+assert dbscan_label[-6] == dbscan_label[-5]
+assert dbscan_label[-4] == dbscan_label[-3]
+assert dbscan_label[-2] == dbscan_label[-1]
+start_time = time.time()
+dbscan_16 = []
+dbscan_64 = []
+dbscan_Q = []
+counter = 0
+for j in range(total_cat):
+    assert j < total_cat
+    temp_set = []
+    counter = 0
+    for i in range(len(dbscan_label)):
+        if dbscan_label[i] == j:
+            temp_set.append(np.append(np.copy(new_dim_points[i]), [i]))
+            counter += 1
+    print(counter)
+    assert counter == total_input/total_cat
+    if dbscan_label[-6] == j:
+        dbscan_16 = np.array(temp_set.copy())
+    if dbscan_label[-4] == j:
+        dbscan_64 = np.array(temp_set.copy())
+    if dbscan_label[-2] == j:
+        dbscan_Q = np.array(temp_set.copy())
+time_dif = get_time_dif(start_time)
+print("Time usage:", time_dif)
+
+#%% compute accuracy
+incorrect16 = max(len(np.setdiff1d(dbscan_16[:, 2], label_16[:, 2])),
+              len(np.setdiff1d(label_16[:, 2], dbscan_16[:, 2]))) 
+incorrect64 = max(len(np.setdiff1d(dbscan_64[:, 2], label_64[:, 2])), 
+              len(np.setdiff1d(label_64[:, 2], dbscan_64[:, 2])))
+incorrectQ = max(len(np.setdiff1d(dbscan_Q[:, 2], label_Q[:, 2])), 
+              len(np.setdiff1d(label_Q[:, 2], dbscan_Q[:, 2])))
+correct = total_input - total_spy - incorrect16 - incorrect64 - incorrectQ
+accuracy = correct / (total_input - total_spy)
+print(accuracy)
+#%%
+print('Plot TSNE by label...')
+start_time = time.time()
+fig, ax = plt.subplots()
+print_tsne(data=label_16, label='16', color='dodgerblue', axis=ax)
+print_tsne(data=label_64, label='64', color='red', axis=ax)
+print_tsne(data=label_Q, label='Q', color='gold', axis=ax)
+plt.show()
+time_dif = get_time_dif(start_time)
+print("Time usage:", time_dif)
+
+#%%
+print('Plot TSNE by dbscan...')
+start_time = time.time()
+fig, ax = plt.subplots()
+print_tsne(data=dbscan_16, label='16', color='dodgerblue', axis=ax)
+print_tsne(data=dbscan_64, label='64', color='red', axis=ax)
+print_tsne(data=dbscan_Q, label='Q', color='gold', axis=ax)
+plt.show()
+time_dif = get_time_dif(start_time)
+print("Time usage:", time_dif)
+
+
+#%% useless
+#for i in point_list_64:
+#    if i[0] < 0 and i[1] >15:
+#        print(i[2])
+
+
+
 
 ##test temp
 # print('Getting test errors...')
@@ -155,3 +287,5 @@ print("Time usage:", time_dif)
 # plt.show()
 # time_dif = get_time_dif(start_time)
 # print("Time usage:", time_dif)
+
+
--- tfrbm/cgbrbm.py
+++ tfrbm/cgbrbm.py
@@ -14,7 +14,15 @@ class CGBRBM(GBRBM):
                  n_visible=8192,
                  n_hidden=800,
                  chanel_increase=16,
-                 filter_sizes=[5, 5],  # 卷积核尺寸
+                 filter_height=5,  # 卷积核尺寸
+                 filter_width=5,  # 卷积核尺寸
+                 offset_height=80,
+                 offset_width=270,
+                 target_height=700,
+                 target_width=700,
+                 resize_height=64,
+                 resize_width=64,
+                 resize_method=0,  # 0:BILINEAR; 1:NEAREST_NEIGHBOR; 2:BICUBIC; 3:AREA
                  **kwargs):
 
         # n_visible                 显层个数
@@ -23,14 +31,19 @@ class CGBRBM(GBRBM):
         # chanel_increase           chanel 第一层之后的增加个数，第二层之后每次变两倍
         # filter_sizes = [5， 5]     卷积核尺寸
 
+        # convolution layer and image attributes
         self.n_visible = n_visible
-        self.matrix_counter = 0
         self.chanel_increase = chanel_increase
-        self.filter_sizes = filter_sizes
-        self.raw_input_x = tf.placeholder(tf.string, [None], name="input_x")
-        self.raw_input_y = tf.placeholder(tf.string, [None], name="input_y")
-        self.conv_input = tf.placeholder(dtype=tf.float32, shape=[None, None, None, None], name="input")
+        self.filter_sizes = [filter_height, filter_width]
+        self.offset_height = offset_height
+        self.offset_width = offset_width
+        self.target_height = target_height
+        self.target_width = target_width
+        self.resize_height = resize_height
+        self.resize_width = resize_width
+        self.resize_method = resize_method
 
+        # set convolution layers' weight and bias
         filter_shape = self.filter_sizes.copy()
         filter_shape.extend([1, self.chanel_increase])
         self.w1 = tf.get_variable(name="w1", shape=filter_shape,
@@ -42,12 +55,18 @@ class CGBRBM(GBRBM):
                                   initializer=tf.initializers.truncated_normal(stddev=0.1))
         self.b2 = tf.get_variable(name='b2', initializer=tf.constant(0.1, shape=[self.chanel_increase * 2]))
 
+        # predefine
+        self.matrix_counter = 0
+        self.raw_input_x = tf.placeholder(tf.string, [None], name="input_x")
+        self.raw_input_y = tf.placeholder(tf.string, [None], name="input_y")
+        self.conv_input = tf.placeholder(dtype=tf.float32, shape=[None, None, None, None], name="input")
+        self.flatten = None
+
         GBRBM.__init__(self, n_visible, n_hidden, **kwargs)
 
     def build_convolution_max_pooling(self):
-        # convolution and max-pooling layers
 
-        pooled_outputs = []
+        # convolution and max-pooling layers
         filter_shape = self.filter_sizes.copy()
         filter_shape.extend([1, self.chanel_increase])
         conv1 = tf.nn.conv2d(self.conv_input, self.w1, strides=[1, 1, 1, 1], padding="SAME", name="conv1")
@@ -62,37 +81,23 @@ class CGBRBM(GBRBM):
 
         self.flatten = tf.reshape(pool2, [-1, self.n_visible])
 
-    @staticmethod
-    def build_image_dataset(input_x,
-                            input_y,
-                            offset_height=80,
-                            offset_width=270,
-                            target_height=700,
-                            target_width=700,
-                            resize_height=64,
-                            resize_width=64,
-                            resize_method=0):  # 0:BILINEAR; 1:NEAREST_NEIGHBOR; 2:BICUBIC; 3:AREA
+    def build_image_dataset(self, input_x,
+                            input_y,):
         image = tf.read_file(input_x)
         decoded_image = tf.image.decode_jpeg(image)
-        cropped_image = tf.image.crop_to_bounding_box(decoded_image, offset_height, offset_width, target_height,
-                                                      target_width)
-        resized_image = tf.image.resize_images(cropped_image, [resize_height, resize_width], resize_method)
+        cropped_image = tf.image.crop_to_bounding_box(decoded_image, self.offset_height, self.offset_width,
+                                                      self.target_height, self.target_width)
+        resized_image = tf.image.resize_images(cropped_image, [self.resize_height, self.resize_width],
+                                               self.resize_method)
         gray_image = tf.image.rgb_to_grayscale(resized_image)
-        one_hot = tf.image.per_image_standardization(gray_image)
-        return one_hot, input_y
+        normalized_image = tf.image.per_image_standardization(gray_image)
+        return normalized_image, input_y
 
     def cfit(self,
              raw_data_x,  # 图片路径，np array
              raw_data_y,  # label, np array
              n_epoches=10,
              batch_size=30,
-             offset_height=80,
-             offset_width=270,
-             target_height=700,
-             target_width=700,
-             resize_height=64,
-             resize_width=64,
-             resize_method=0,
              shuffle=True,
              verbose=True):
         assert n_epoches > 0
@@ -106,24 +111,11 @@ class CGBRBM(GBRBM):
 
         if shuffle:
             dataset = tf.data.Dataset.from_tensor_slices((self.raw_input_x, self.raw_input_y)).map(
-                lambda x, y: self.build_image_dataset(x, y, offset_height=offset_height,
-                                                      offset_width=offset_width,
-                                                      target_height=target_height,
-                                                      target_width=target_width,
-                                                      resize_height=resize_height,
-                                                      resize_width=resize_width,
-                                                      resize_method=resize_method)).shuffle(n_data,
-                                                                                            reshuffle_each_iteration=True).batch(
+                lambda x, y: self.build_image_dataset(x, y)).shuffle(n_data, reshuffle_each_iteration=True).batch(
                 batch_size).repeat(n_epoches)
         else:
             dataset = tf.data.Dataset.from_tensor_slices((self.raw_input_x, self.raw_input_y)).map(
-                lambda x, y: self.build_image_dataset(x, y, offset_height=offset_height,
-                                                      offset_width=offset_width,
-                                                      target_height=target_height,
-                                                      target_width=target_width,
-                                                      resize_height=resize_height,
-                                                      resize_width=resize_width,
-                                                      resize_method=resize_method)).batch(batch_size).repeat(n_epoches)
+                lambda x, y: self.build_image_dataset(x, y)).batch(batch_size).repeat(n_epoches)
 
         iterator = dataset.make_initializable_iterator()
         image_batch_get_next, label_batch_get_next = iterator.get_next()
@@ -197,44 +189,76 @@ class CGBRBM(GBRBM):
 
         return outputs
 
-    def cget_error(self,
-                   data_x,
-                   batch_size=30,
-                   offset_height=50,
-                   offset_width=250,
-                   target_height=750,
-                   target_width=750,
-                   resize_height=256,
-                   resize_width=256,
-                   resize_method=3):
-        n_data = data_x.shape[0]
+    def ctransform_raw(self,
+                       raw_data_x,
+                       raw_data_y,
+                       batch_size):
+        n_data = raw_data_x.shape[0]
 
         if batch_size > 0:
             n_batches = n_data // batch_size + (0 if n_data % batch_size == 0 else 1)
         else:
             n_batches = 1
 
-        errors = np.zeros((n_batches,))
-        error_counter = 0
-
-        dataset = tf.data.Dataset.from_tensor_slices(self.raw_input_x).map(
-            lambda x, y: self.build_image_dataset(x, y, offset_height=offset_height,
-                                                  offset_width=offset_width,
-                                                  target_height=target_height,
-                                                  target_width=target_width,
-                                                  resize_height=resize_height,
-                                                  resize_width=resize_width,
-                                                  resize_method=resize_method)).batch(batch_size)
+        dataset = tf.data.Dataset.from_tensor_slices((self.raw_input_x, self.raw_input_y)).map(
+            lambda x, y: self.build_image_dataset(x, y)).batch(batch_size)
 
         iterator = dataset.make_initializable_iterator()
-        batch_x = iterator.get_next()
-        self.sess.run(iterator.initializer, feed_dict={self.raw_input_x: data_x})
+        image_batch_get_next, label_batch_get_next = iterator.get_next()
+        self.sess.run(iterator.initializer, feed_dict={self.raw_input_x: raw_data_x, self.raw_input_y: raw_data_y})
 
         r_batches = range(n_batches)
+        outputs = np.zeros((n_data, self.n_hidden))
+        output_counter = 0
+        spy_file_counter = 0
         for _ in r_batches:
-            batch_x = self.sess.run(self.flatten, feed_dict={self.conv_input: self.sess.run(batch_x)})
-            error = self.get_err(batch_x)
-            errors[error_counter] = error
-            error_counter += 1
+            feature_temp, label_temp = self.sess.run([image_batch_get_next, label_batch_get_next])
+            convolved_images = self.sess.run(self.flatten, feed_dict={self.conv_input: feature_temp})
 
-        return errors
+            with open('./pickle/feature_spy' + str(spy_file_counter) + '.pkl', 'wb') as p:
+                pickle.dump(convolved_images, p)
+            with open('./pickle/label_spy' + str(spy_file_counter) + '.pkl', 'wb') as p:
+                pickle.dump(label_temp, p)
+            spy_file_counter += 1
+            output = self.transform(convolved_images)
+            for i in range(len(output)):
+                outputs[output_counter] = output[i]
+                output_counter += 1
+
+        return outputs
+
+    # def cget_error(self,
+    #                data_x,
+    #                batch_size=30,
+    #                offset_height=50,
+    #                offset_width=250,
+    #                target_height=750,
+    #                target_width=750,
+    #                resize_height=256,
+    #                resize_width=256,
+    #                resize_method=3):
+    #     n_data = data_x.shape[0]
+    #
+    #     if batch_size > 0:
+    #         n_batches = n_data // batch_size + (0 if n_data % batch_size == 0 else 1)
+    #     else:
+    #         n_batches = 1
+    #
+    #     errors = np.zeros((n_batches,))
+    #     error_counter = 0
+    #
+    #     dataset = tf.data.Dataset.from_tensor_slices(self.raw_input_x).map(
+    #         lambda x, y: self.build_image_dataset(x, y)).batch(batch_size)
+    #
+    #     iterator = dataset.make_initializable_iterator()
+    #     batch_x = iterator.get_next()
+    #     self.sess.run(iterator.initializer, feed_dict={self.raw_input_x: data_x})
+    #
+    #     r_batches = range(n_batches)
+    #     for _ in r_batches:
+    #         batch_x = self.sess.run(self.flatten, feed_dict={self.conv_input: self.sess.run(batch_x)})
+    #         error = self.get_err(batch_x)
+    #         errors[error_counter] = error
+    #         error_counter += 1
+    #
+    #     return errors
